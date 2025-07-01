ea8077e0faacf5d911b3da8bf2f20d7dbea5af5b
AbdelrahmanElawady <abdoelawady125@gmail.com>
2025-07-01 17:45:50 +0300

    fix: set fallback for SPA

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		})
 	}
 };
-
-export default config;
